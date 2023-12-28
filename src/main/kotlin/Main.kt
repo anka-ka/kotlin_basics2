370d4cd8b87e647e293eec88483bf403acd0965d
kaz <kazan19066@gmail.com>
2023-12-28 23:23:03 +0400

    Second fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import java.util.*
 fun main() {
     println("Введите колиство лайков")
     val likes = readln().toInt()
-    if (likes % 100 == 11 || likes % 10 in 2..9) {
+    if (likes % 100 == 11 || likes % 10 in 2..9 || likes % 10 == 0) {
         println("Понравилось $likes людям")
     } else if (likes % 10 == 1) {
         println("Понравилось $likes человеку")
